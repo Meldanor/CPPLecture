db32c0691ebedd68feb150b8ed4ad5d8b941d6be
Meldanor <Bhaals@gmx.de>
2013-07-02 15:44:10 +0200

    Complete template task

--- CPPLecture/collection/arraylist.hpp
+++ CPPLecture/collection/arraylist.hpp
@@ -2,57 +2,65 @@
 
 #include "collection.hpp"
 
-namespace Collections
-{
+namespace Collections {
 	// TODO: Dynamical template array class: ArrayList
 	//  - implement interface
 	//  - reserve
 	//  - constructor
 	//	- constructor from other collection
 	//	- operator[]
-	template<typename T> class ArrayList: public ICollection<T>
-	{
+	template<typename T> class ArrayList: public ICollection<T> {
 	public:
 
         ArrayList() {
-
+            m_numElements = 0;
+            capacity = 16;
+            array = new T[capacity];
         }
 
         ArrayList(ICollection& collection) {
+            m_numElements = 0;
+            capacity = collection.getNumElements() * 2;
+            array = new T[capacity];
 
+	        auto it = collection.getIterator();
+	        while(it->current())
+	        {
+                insert(*(it->current()));
+		        it->next();
+	        }
         }
-        
-		// TODO: constructors
 
-		virtual ~ArrayList()
-		{
-			// TODO
+		virtual ~ArrayList() {
+            delete[] array;
 		}
 
-		void resize( unsigned int numInstances )
-		{
-			// TODO
+		void resize( unsigned int numInstances ) {
+            T* newArray = new T[numInstances];
+            for (unsigned int i = 0; i < m_numElements ; ++i) {
+                newArray[i] = array[i];
+            }
+            delete[] array;
+            array = newArray;
+            capacity = numInstances;
 		}
 
-		virtual void insert( const T& newElement ) override
-		{
-			// TODO
+		virtual void insert( const T& newElement ) override {
+            if (m_numElements == capacity)
+                resize(capacity * 2);
+            array[m_numElements++] = newElement;
 		}
-
-		// TODO: Operator []
 
         T& ArrayList::operator[](unsigned int const index) {
             return array[index];
         }
 
-		class Iterator: public ICollection<T>::IIterator
-		{
+		class Iterator: public ICollection<T>::IIterator {
 		public:
 			Iterator( T* first, int numElements ) : 
 				m_array( numElements ? first : nullptr ), m_remaining( numElements )		{}
 
-			virtual T* next() override
-			{
+			virtual T* next() override {
 				if( m_remaining > 1 )
 				{
 					--m_remaining;
@@ -65,8 +73,7 @@ namespace Collections
 
 			/// \brief Access the current element without changing the iterator
 			///
-			virtual T* current() const override
-			{
+			virtual T* current() const override {
 				return m_array;
 			}
 
@@ -76,12 +83,12 @@ namespace Collections
 			int m_remaining;
 		};
 
-		virtual std::shared_ptr<typename ICollection<T>::IIterator> getIterator() const override
-		{
-            return std::make_shared<Iterator>(array,0);
+		virtual std::shared_ptr<typename ICollection<T>::IIterator> getIterator() const override {
+            return std::make_shared<Iterator>(array, m_numElements);
 		}
 
 	private:
         T* array;
+        unsigned int capacity;
 	};
 };
